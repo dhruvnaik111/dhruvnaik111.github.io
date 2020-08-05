84cc94b38f635668424b4e2ca5993994d69078f2
dhruvnaik111 <68032436+dhruvnaik111@users.noreply.github.com>
2020-08-05 08:34:38 -0700

    router workz

--- koaanwebsitereact/src/App.js
+++ koaanwebsitereact/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { HashRouter as Router, ROute, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  ROute,
+  Switch,
+  HashRouter,
+  Route,
+} from "react-router-dom";
+import { Home } from "./pages/HomePage/Home.js";
+import { About } from "./pages/AboutPage/About.js";
+import { Contact } from "./pages/ContactPage/Contact.js";
 
 function App() {
   return (
     <React.Fragment>
-      <h1>hello</h1>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </HashRouter>
     </React.Fragment>
   );
 }
